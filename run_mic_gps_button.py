1a3090f24c62bac2930d1c8156d90c977163f01a
Nataliia Sergiienko <nataliia.sergiienko@gmail.com>
2018-04-01 16:50:27 +0930

    Fix various bugs in gps readings

--- run_mic_gps_button.py
+++ run_mic_gps_button.py
@@ -4,6 +4,7 @@
 # ReSpeaker Mic Array - 8 channels, 16 kHz, 16 bit little endian
 # GPS - 1 Hz, date, time (UTC), latitude, longitude
 
+import math
 import time
 import subprocess
 import os
@@ -34,51 +35,72 @@ def startMic():
 
     global micProcess
     micProcess = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, preexec_fn=os.setsid)
-    print("Started recording mic")
+    print('Started recording mic')
 
 # Stop recording data from mics
 def stopMic():
     os.killpg(os.getpgid(micProcess.pid), signal.SIGTERM)
-    print("Stopped recording mic")
-#    for channel in range (1, 9):
-#        tfm = sox.Transformer()
-#        remix_dictionary = {1: [channel]}
-#        tfm.remix(remix_dictionary)
-#        filename_mic_channel = (filename_mic[:-4] + '-ch-' + str(channel) + '.wav')
-#        tfm.build(filename_mic, filename_mic_channel)
-#    print("Splitted 8 channels to separate files")
+    print('Stopped recording mic')
+
+# Split written data from mics
+def splitMic():
+    print('Splitting written mic channels...')
+    for channel in range (1, 9):
+        tfm = sox.Transformer()
+        remix_dictionary = {1: [channel]}
+        tfm.remix(remix_dictionary)
+        filename_mic_channel = (filename_mic[:-4] + '-ch-' + str(channel) + '.wav')
+        tfm.build(filename_mic, filename_mic_channel)
+    print('Splitted 8 channels to separate files')
 
 # Start writing GPS data
 def startGps():
-    print("Started writing GPS data")
+
+    # initialisation
+    print('Started writing GPS data')
     timestr_gps = time.strftime("%Y%m%d-%H%M%S")
     filename_gps = ('gps-data-' + timestr_gps + '.csv')
-
-    global file_csv 
     file_csv = open(filename_gps, "w")
-    file_csv.write('Date (RPi), Time (RPi), Date (GPS), UTC (GPS), Latitude, Longitude, Validity \n')
+    file_csv.write('Date (RPi), Time (RPi), Date (GPS), UTC (GPS), Latitude, Longitude, Validity\n')
     gps = microstacknode.hardware.gps.l80gps.L80GPS()
-    while isRunning: #pr.poll() is None:
+
+    # reading loop
+    before_msec = 0
+    while isRunning:
+
+        # always read from GPS as often as possible
+        data = None
+        err = None
         try:
             data = gps.get_gprmc()
-            file_csv.write(time.strftime("%d/%m/%Y,%H:%M:%S,") + 
-                           str(data['date']) + ',' + str(data['utc']) + ',' + 
-                           str(data['latitude']) + ',' + str(data['longitude']) + ',' + str(data['data_valid']) + "\n")
-#            time.sleep(1)
-        except Exception: # as inst:
-#            file_csv.write(str(inst) + "\n")
-#            time.sleep(1)
+        except Exception as inst:
+            err = inst
             pass
-    else:
-        file_csv.close()
 
-# Stop writing GPS data
-def stopGps():
-    try:
-        file_csv.close()
-    except Exception:
-        pass
-    print("Stopped writing GPS data")
+        # ignore readings if less than a second elapsed
+        now_msec = math.floor(time.monotonic() * 1000)
+        if now_msec - before_msec < 1000:
+            continue
+        else:
+            before_msec = now_msec
+
+        # write either data or an error
+        if err is not None:
+            file_csv.write(str(err) + '\n')
+        else:
+            file_csv.write(time.strftime("%d/%m/%Y,%H:%M:%S,") +
+                           str(data['date']) + ',' +
+                           str(data['utc']) + ',' +
+                           str(data['latitude']) + ',' +
+                           str(data['longitude']) + ',' +
+                           str(data['data_valid']) + '\n')
+
+        # force writing to disk
+        file_csv.flush()
+
+    # clean up
+    file_csv.close()
+    print('Stopped writing GPS data')
 
 # Logic when pushbutton is ON/OFF
 def onButtonPress(channel):
@@ -88,17 +110,17 @@ def onButtonPress(channel):
     if (isRunning):
         pixel_ring.on()
         startMic()
-        t = threading.Thread(target=startGps)
-        t.start()
+        threading.Thread(target=startGps).start()
     else:
         pixel_ring.off()
         stopMic()
-        stopGps()
+        splitMic()
 
 # Monitoring the status of pushbutton
 GPIO.add_event_detect(PIN_ID, GPIO.RISING, callback=onButtonPress, bouncetime=1000)
 
 # Run forever as a service
+print('Waiting for push button presses...\n')
 while True:
     pass
 
